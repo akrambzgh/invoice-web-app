c28d48f84bf890a78149cdd3219c0a690a61038d
Akram Bouzoualegh <89752239+akrambzgh@users.noreply.github.com>
2023-06-07 20:20:12 +0100

    Set up the environment with webpack

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./scss/main.scss";
+
+let h1 = document.querySelector("h1");
+
+h1?.addEventListener("click", () => {
+  console.log("helloo");
+});
+console.log("welcome");
---
+++ webpack.common.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+};
